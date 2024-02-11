33280674ed0a026f409803c9ff51c776aca8aea6
tomo25 <tomotomo2525neko@gmail.com>
2024-02-11 21:20:38 +0900

    現在の初期コード

--- src/main/java/org/tomo25/snowballfight/SnowballFightListener.java
+++
@@ -1,9 +0,0 @@
-package org.tomo25.snowballfight;
-
-import org.bukkit.event.Listener;
-
-public class SnowballFightListener implements Listener {
-    public SnowballFightListener(SnowballFightManager snowballFightManager) {
-        // コンストラクタが必要な場合、追加
-    }
-}
--- src/main/java/org/tomo25/snowballfight/SnowballListener.java
+++
@@ -1,9 +0,0 @@
-package org.tomo25.snowballfight;
-
-import org.bukkit.event.Listener;
-
-public class SnowballListener implements Listener {
-    public SnowballListener(SnowballFightManager snowballFightManager, SnowballFight plugin) {
-        // コンストラクタが必要な場合、追加
-    }
-}
